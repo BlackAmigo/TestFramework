9ac7f1c433283c2461489066b1abffb66a0ba4d0
BlackAmigo <anatoliy.mir.pro@gmail.com>
2019-12-09 23:29:59 +0700

    Cucumber added

---
+++ src/test/java/ru/autotests/cucumber/CucumberRunnerTest.java
@@ -0,0 +1,19 @@
+package ru.autotests.cucumber;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/ru/autotests/cucumber/features",
+        glue = "ru/autotests/cucumber/steps",
+//        tags = "@withdrawal",
+//        dryRun = false,
+//        strict = false,
+        snippets = SnippetType.CAMELCASE
+//        name = "^Успешное|Успешная.*"
+)
+public class CucumberRunnerTest {
+}
---
+++ src/test/java/ru/autotests/cucumber/steps/StepDefinition.java
@@ -0,0 +1,36 @@
+package ru.autotests.cucumber.steps;
+
+import cucumber.api.java.ru.Допустим;
+import cucumber.api.java.ru.Если;
+import cucumber.api.java.ru.Тогда;
+import ru.autotests.pages.yandex.MainPage;
+import ru.autotests.webdriver.WebDriverManager;
+
+import static ru.autotests.webdriver.WebDriverManager.DriverType.CHROME;
+
+public class StepDefinition {
+
+    private MainPage mainPage;
+
+    @Допустим("^, что открыта страница https://yandex\\.ru$")
+    public void чтоОткрытаСтраницаHttpsYandexRu() {
+        WebDriverManager.setupWebDriver(CHROME);
+        mainPage = new MainPage();
+        mainPage.open();
+    }
+
+    @Если("^ввести в поле поиска \"([^\"]*)\"$")
+    public void ввестиВПолеПоиска(String query) {
+        mainPage.setSearchField(query);
+    }
+
+    @Если("^нажать на кнопку 'Найти'$")
+    public void нажатьНаКнопкуНайти() {
+        mainPage.clickSearchButton();
+    }
+
+    @Тогда("^искать ссылку с текстом \"([^\"]*)\", на первых \"([^\"]*)\" страницах$")
+    public void искатьСсылкуСТекстомНаПервыхСтраницах(String siteName, int count) {
+        mainPage.findPageInResponse(siteName, count);
+    }
+}
